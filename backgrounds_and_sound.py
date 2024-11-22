3e302e008504d09518681998825c5f74cb97e7cc
Dang-Huy <dangnhathuy0401@gmail.com>
2024-11-22 15:55:53 +0700

    Update backgrounds_and_sound.py

--- backgrounds_and_sound.py
+++ backgrounds_and_sound.py
@@ -1,13 +1,11 @@
 import pygame
-import os
-
-os.chdir(os.path.dirname(__file__))
+import random, time
 menu_bg = pygame.image.load("menu_bg.png")
-menu_bg = pygame.transform.scale(menu_bg, (800, 700))  # Updated size
+menu_bg = pygame.transform.scale(menu_bg, (800, 700)) 
 gameplay_bg = pygame.image.load("gameplay_bg.png")
-gameplay_bg = pygame.transform.scale(gameplay_bg, (800, 700))  # Updated size
+gameplay_bg = pygame.transform.scale(gameplay_bg, (800, 700)) 
 
-is_muted = False  # Global variable to track mute state
+is_muted = False  
 
 def sound():
     pygame.mixer.init()
@@ -30,3 +28,17 @@ def toggle_mute():
     else:
         pygame.mixer.music.set_volume(0.0)
         is_muted = True
+
+def show_bubbles(screen):
+    start_time = time.time()
+    screen_rect = screen.get_rect()
+    bubbles = pygame.image.load('bubbles.png')  # Load your bubbles image
+    bubbles = pygame.transform.scale(bubbles, (40, 40))  # Resize bubbles to be larger
+
+    while time.time() - start_time < 2:
+        for _ in range(5):  # Show 5 bubbles at random positions
+            x = random.randint(0, screen_rect.width - bubbles.get_width())
+            y = random.randint(0, screen_rect.height - bubbles.get_height())
+            screen.blit(bubbles, (x, y))
+        pygame.display.flip()  # Update the full display Surface to the screen
+        pygame.time.delay(100)  # Delay to control the update speed
